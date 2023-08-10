163ec4755209b70d8ca0f2bc4c704a8ee066147c
zuhairm2001 <zuhairm2001@hotmail.com>
2023-08-10 17:34:07 +1000

    firebase

---
+++ portfolio-app/src/Components/TitleComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TitleComponent() {
+  return (
+    <div>
+        <h2>
+            hi, my name is
+        </h2>
+        <h2>
+            zuhair malik
+        </h2>
+    </div>
+  )
+}
+
+export default TitleComponent
