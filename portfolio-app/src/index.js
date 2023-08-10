de111183597e271ff17bbe0e1261708f881457db
zuhairm2001 <zuhairm2001@hotmail.com>
2023-08-10 17:34:13 +1000

    firebase setup

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,8 +14,6 @@ root.render(
   </React.StrictMode>
 );
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
