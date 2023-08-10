ee19af90446a7865560b786f95ff881da0601119
zuhairm2001 <zuhairm2001@hotmail.com>
2023-08-10 17:12:53 +1000

    firebase + title component

--- portfolio-app/src/index.js
+++ portfolio-app/src/index.js
@@ -11,6 +11,27 @@ root.render(
   </React.StrictMode>
 );
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBP8GJ3Z_E4C-o7USW8cCmlyV1zk_vNjY",
+  authDomain: "portfolioproject-395506.firebaseapp.com",
+  projectId: "portfolioproject-395506",
+  storageBucket: "portfolioproject-395506.appspot.com",
+  messagingSenderId: "1025939481054",
+  appId: "1:1025939481054:web:2ccf0c2bc2345ba77cbd4f",
+  measurementId: "G-4BQSYH84P2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
