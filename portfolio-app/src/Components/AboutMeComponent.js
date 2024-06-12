d55d4da53a6f0fee039ab68146c8e01493d54d10
zuhairm2001 <zuhairm2001@hotmail.com>
2024-06-12 14:49:10 +1000

    completed about me component

--- portfolio-app/src/Components/AboutMeComponent.js
+++ portfolio-app/src/Components/AboutMeComponent.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "animate.css/animate.compat.css"
 import ScrollAnimation from 'react-animate-on-scroll'
+import images from "../ImageList";
+
 
 function AboutMeComponent(props) {
 
 
     return<>
 
-        <div className="grid grid-cols-2 pt-10 w-fit">
+        <div className="grid grid-cols-2 pt-10 pb-10 w-fit">
             <div className="flex justify-end pr-10">
-                <img src="https://cdn.myanimelist.net/images/characters/7/83946.jpg" className="justify-start" alt="aboutme"/>
+                <img src="/aboutMe.jpg" className="justify-start rounded-full" alt="aboutme" height={300} width={300}/>
             </div>
             <div className="w-1/2 ">
                 <p className="text-center text-lg font-normal tracking-tight text-white">
                     Welcome to my portfolio! I'm a Computer Science student at the University of Technology Sydney (UTS),
-                    majoring in Artificial Intelligence and am currently completing my honours year. My passion lies in
-                    both general software development and the innovative world of AI. Here, you can explore my projects,
+                    majoring in <b>Artificial Intelligence</b> and am currently completing my honours year. My passion lies in
+                    both general software development and the innovative world of AI.
+                </p>
+                <p className="text-center text-lg font-normal tracking-tight text-white pt-3">
+                    Here, you can explore my projects,
                     skills, and experiences that showcase my journey and expertise in creating intelligent solutions and
                     robust software applications. Dive in to see how I merge creativity with technology to solve complex
                     problems and drive progress in the tech industry.
@@ -25,50 +30,55 @@ function AboutMeComponent(props) {
         </div>
 
         <ScrollAnimation animateIn="fadeIn" duration={2}>
-            <div className="grid grid-cols-2 pt-10 w-fit">
-                <img src="../../public/aboutMe.jpg" alt="aboutMe"/>
-                <div className="w-1/2 ">
-                    <p className="text-center text-lg font-normal tracking-tight text-white">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                        into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                        software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
+            <h1 className="justify-center text-4xl font-bold tracking-tight text-white pb-6 pt-6">
+                languages
+            </h1>
+            <div className="grid grid-cols-4">
+                {images.map(image => (
+                    <img src={image} key={image} alt="image" height='150' width='150'
+                         className="p-4"/>
+                ))}
             </div>
+
+
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeIn" duration={2}>
-            <div className="grid grid-cols-2 pt-10 w-fit">
-                <img src="../../public/aboutMe.jpg" alt="aboutMe"/>
-                <div className="w-1/2 ">
-                    <p className="text-center text-lg font-normal tracking-tight text-white">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                        into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                        software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
-            </div>
+            <h1 className="justify-center text-4xl font-bold tracking-tight text-white pb-6 pt-6">
+                experience
+            </h1>
+            <h1 className="justify-center text-2xl font-bold tracking-tight text-white pb-6 pt-6">
+                - risk professionals | 2022-present
+            </h1>
+            <h1 className="justify-center text-xl font-normal tracking-tight text-white pb-6 pt-6">
+                lead developer and manager
+            </h1>
+            <ul className="justify-start text-lg font-normal tracking-tight text-white pb-6">
+                <li>- founding member</li>
+                <li>- developed website interface using wordpress stack</li>
+                <li>- liason for partner companies</li>
+                <li>- first contact for b2b and b2c</li>
+                <li>- in charge of social media and graphic creation</li>
+                <li>- developed tool to conduct current state assessment/audit for ISO22301, CPS234 and CPS230 </li>
+
+            </ul>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeIn" duration={2}>
-            <div className="grid grid-cols-2 pt-10 w-fit">
-                <img src="../../public/aboutMe.jpg" alt="aboutMe"/>
-                <div className="w-1/2 ">
-                    <p className="text-center text-lg font-normal tracking-tight text-white">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                        into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                        release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                        software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
-                </div>
-            </div>
+            <h1 className="justify-center text-2xl font-bold tracking-tight text-white pb-6 pt-6">
+                - loanoptions.ai | 2022 feb-may
+            </h1>
+            <h1 className="justify-center text-xl font-normal tracking-tight text-white pb-6 pt-6">
+                data science and software intern
+            </h1>
+            <ul className="justify-start text-lg font-normal tracking-tight text-white pb-6">
+                <li>- participated in agile work environment</li>
+                <li>- resolved a major roadblock pertaining to data collection and analysis for a potential new feature</li>
+                <li>- optimised driver license OCR program to improve functionality and efficiency for broker dashboard</li>
+                <li>- suggested and developed an document search tool with nlp to improve broker efficiency</li>
+
+
+            </ul>
         </ScrollAnimation>
     </>;
 }
+
 export default AboutMeComponent;
---
+++ portfolio-app/src/ImageList.js
@@ -0,0 +1,20 @@
+// import CSharp from '../public/Languages/C#.png'
+// import Java from '../public/Languages/Java.png';
+// import JS from "../public/Languages/JS.png";
+// import Python from "../public/Languages/Python.png";
+
+// let images = [
+//     CSharp,
+//     Java,
+//     JS,
+//     Python
+// ]
+
+let images = [
+    '/C%23.png',
+    '/JS.png',
+    '/Python.png',
+    '/Java.png',
+]
+
+export default images;
